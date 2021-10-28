fe1f86ec835fad6bde014bb708ed6ef1174406f6
cavin <xjr30226@126.com>
2021-10-28 14:51:10 +0800

    js功能代码

---
+++ js/index.js
@@ -0,0 +1,60 @@
+function makeData(num) {
+    var data = Array();
+    for (var i = 1; i <= num; i++) {
+        let d = new Object();
+        d.id = i;
+        d.name = `第${i}节`;
+        data.push(d);
+    }
+    return data;
+}
+
+function getDate() {
+    var d = new Date();
+    let year = d.getFullYear();
+    let mon = d.getMonth() + 1;
+    let day = d.getDate();
+    return year + '-' + mon + '-' + day;
+}
+
+var app = new Vue({
+  el: '#main-container',
+  data: {
+    items: makeData(64),
+    pushup: 0,
+    courseName: "B站气象学课程",
+    whatched: [],
+    today: getDate(),
+  },
+  methods: {
+    onSubmit() {
+        axios.post('/daily/api', 
+            {
+                courseName: this.courseName,
+                whatched: this.whatched,
+                pushup: this.pushup
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        ).then(function (response) {
+            alert("更新成功！");
+        }).catch(function(error) {
+            console.log(error);
+        });
+    },
+    getData() {
+        axios.get('/daily/get_data').then((response)=>{
+            let data = response.data;
+            this.apiData = data;
+            let whatched = data.course.watched.split(",");
+            this.whatched = whatched.map(item => parseInt(item, 10));
+        })
+    }
+  },
+  mounted: function() {
+      this.getData();
+  }
+})
